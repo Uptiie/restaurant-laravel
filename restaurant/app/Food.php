25abdb0b2787b88942879abd60b3ca3e3efc01be
Donovan Langager <donovan.lan16@gmail.com>
2020-08-23 14:37:17 -0700

    added food and category controller + views

---
+++ restaurant/app/Food.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+//take models from migrate file and turn them into fillable properties
+
+class Food extends Model
+{
+    protected $fillable=['name', 'description', 'price', 'category_id' ];
+}
